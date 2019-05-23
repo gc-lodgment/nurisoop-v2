80fe0b98058b5616ea0236480f593d856d6623af
som <d.sssom13@gmail.com>
2019-05-23 12:14:20 +0900

    ing

---
+++ src/js/common.js
@@ -0,0 +1,77 @@
+$(function(){
+    //함수실행
+    ftSlideFn();
+    tabFn();
+    
+    //search 버튼 클릭 시 
+    $('a[class*="btn-srch"]').on('click', function(){
+        $(this).parents('.srch-box').toggleClass('on');
+    });
+    
+    //메뉴
+    $('.btn-menu').on('click', function(){
+        $(this).toggleClass('on');
+        $('.menu-area').stop().fadeToggle();
+    });
+    
+});
+
+/* tab */
+function tabFn(){
+    $(".tab-list > li").on("click", function() {
+        
+		$(this).addClass('on').siblings().removeClass('on');
+        
+        if( $(this).parents('.tab-box').hasClass('prdList') ){
+            //상품리스트 - sub
+        }else {
+            //ex) 메인 탭 상품 리스트
+            var atr = $(this).find('a').attr('href');
+            var ht = $('header').height();
+            var offsetTop = $(this).parents('.tab-box').offset().top;
+            
+            $(this).parents('.tab-box').siblings('div[class*="-area"]').stop().fadeOut(500);
+            $(atr).stop().fadeIn(700);
+
+            $('html, body').stop().animate({
+                scrollTop : (offsetTop-(ht+15))
+            }, 500);
+        }
+    });
+}
+
+ 
+/* footer family list */
+var fsite = {
+	go_family: function(url){
+		window.open(url);
+		fsite.click_family();
+	}, 
+
+	click_family: function(){
+		var hh = $("#family_list").attr("class");
+		if(hh == "f_on"){
+			$("#family_list").fadeOut();
+			$("#family_list").removeClass("f_on");
+			$(".family-btn-box").removeClass("on");
+		}else{
+			$("#family_list").fadeIn();
+			$("#family_list").addClass("f_on");
+			$(".family-btn-box").addClass("on");
+		}
+	}
+}
+
+/* footer slider */
+function ftSlideFn(){
+    $('#slideFoot').bxSlider({
+        mode: 'fade',
+        auto: true,
+        autoControls: false,
+        autoHover: true,
+        controls: false,
+        pager: true,
+        speed: 1000,
+        duration: 6000
+    });
+}
--- src/js/main/init.js
+++ src/js/main/init.js
@@ -1,3 +1,39 @@
 $(function () {
-    
+    //함수실행
+    mainSlideFn();
+    midSlideFn();
 });
+
+
+/* main slider */
+function mainSlideFn(){
+    $('#mainVisual').bxSlider({
+        mode: 'fade',
+        auto: true,
+        autoHover: true,
+        autoControls: false,
+        controls: true,
+        pager: true,
+        pagerCustom: '#viualPager',
+        speed: 1000,
+        duration: 6000,
+        prevText: '<img src="http://static.nurisoop.co.kr/img/renew/common/btn_arw_left_01.png" alt="이전">',
+        nextText: '<img src="http://static.nurisoop.co.kr/img/renew/common/btn_arw_right_01.png" alt="다음">',
+    });
+}
+
+/* mid slider */
+function midSlideFn(){
+    $('#midSilde').bxSlider({
+        mode: 'fade',
+        auto: true,
+        autoHover: true,
+        autoControls: false,
+        controls: true,
+        pager: false,
+        speed: 1000,
+        duration: 6000,
+        prevText: '<img src="http://static.nurisoop.co.kr/img/renew/common/btn_arw_left_02.png" alt="이전">',
+        nextText: '<img src="http://static.nurisoop.co.kr/img/renew/common/btn_arw_right_02.png" alt="다음">',
+    });
+}
